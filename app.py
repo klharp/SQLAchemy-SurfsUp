bb2dff72ff8dd5f5c3c2f542061e250d95a66436
kerryharp <kharp@umn.edu>
2021-03-12 13:43:12 -0600

    app.py changes for tutor session discussion

--- app.py
+++ app.py
@@ -42,11 +42,13 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/start<br/>"
+        f"/api/v1.0/startQ<br/>"
         f"/api/v1.0/start/end"
     )
 
-
+# -------------------
 # precipitation route
+# -------------------
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
@@ -75,7 +77,9 @@ def precipitation():
     return jsonify(all_query)
 
 
+# -------------------
 # stations route
+# -------------------
 @app.route("/api/v1.0/stations")
 def stations():
 
@@ -94,7 +98,9 @@ def stations():
     return jsonify(stations)
 
 
-# stations route
+# -------------------
+# preciptitation route -- HOW TO DEFINE THE START DATE AS A VARIABLE, SAME PROBLEM IN THE JUPYTER NOTEBOOK
+# -------------------
 @app.route("/api/v1.0/tobs")
 def tobs():
 
@@ -112,12 +118,75 @@ def tobs():
 
     # Convert list of tuples into normal list
     # class 10-advanced-data-storage, 10-Ins_Flask_with_ORM
-    query_list = list(np.ravel(query_data2))
+    query_list2 = list(np.ravel(query_data2))
 
 
     # Return JSON representation1
-    return jsonify(query_list)
+    return jsonify(query_list2)
 
+
+# -------------------
+# start date route -- INCOMPLETE, NEED TO DEFINE THE START DATE 
+# -------------------
+@app.route("/api/v1.0/start")
+def start():
+
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, max temperature from the start"""
+    # Query station data
+
+    query_data3 = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs), func.avg(Measurement.tobs)).all()
+    
+    session.close()
+
+    # Convert list of tuples into normal list
+    # class 10-advanced-data-storage, 10-Ins_Flask_with_ORM
+    query_list3 = list(np.ravel(query_data3))
+    
+    # Return JSON representation1
+    return jsonify(query_list3)
+
+# -------------------
+@app.route("/api/v1.0/startQ")
+# -------------------
+def help():
+
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    print("Is this giving the min, max, average for the entire dataset? How do I specify the start date?")
+    return (
+        f"Is this giving the min, max, average for the entire dataset?<br/>" 
+        f"How do I specify the start date?"
+    )
+
+    session.close()
+
+
+# -------------------
+# start_end date route -- INCOMPLETE, NEED TO DEFINE THE START AND END DATES
+# -------------------
+@app.route("/api/v1.0/start/end")
+def end():
+
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, max temperature from the start"""
+    # Query station data
+
+    query_data4 = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs), func.avg(Measurement.tobs)).all()
+    
+    session.close()
+
+    # Convert list of tuples into normal list
+    # class 10-advanced-data-storage, 10-Ins_Flask_with_ORM
+    query_list4 = list(np.ravel(query_data4))
+    
+    # Return JSON representation1
+    return jsonify(query_list4)
 
 
 
